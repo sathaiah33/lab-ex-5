78843a7c3def1078682d6e83f7f612029934c5f9
sathaiah33 <88027170+sathaiah33@users.noreply.github.com>
2021-07-27 14:04:36 +0530

    Add files via upload

---
+++ q1.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab5;
+import java.util.Scanner;
+/**
+ *
+ * @author rakesh
+ */
+public class q1
+{
+    
+ public static void main(String[] args) {
+        // TODO code application logic here
+        college c[]=new college[10];
+        
+        for(int i=0;i<10;i++)
+        {
+        c[i]=new college(); 
+        c[i].getcollegedetails();
+        c[i].show();
+        }
+    }
+    
+}
+class student
+{
+    String stdname;
+    Scanner obj=new Scanner(System.in);
+    public void getstddetails()
+    {
+        System.out.println("Enter student name");
+       stdname =obj.next(); 
+    }
+    public void show()
+    {
+     System.out.println("Student name:"+stdname);
+    }
+}
+class department extends student
+{
+    String deptname;
+    Scanner j=new Scanner(System.in);
+    public void getdeptdetails()
+    {
+        super.getstddetails();
+        System.out.println("Enter dept name");
+        deptname =j.next();
+        
+    }
+    @Override
+    public void show()
+    {
+        super.show();
+        System.out.println("department name:"+deptname);
+    }
+}
+class college extends department
+{
+    String clgname;
+    Scanner b=new Scanner(System.in);
+    public void getcollegedetails()
+    {
+        super.getdeptdetails();
+        System.out.println("Enter college name");
+        clgname=b.next();
+    }
+    @Override
+    public void show()
+    {
+      super.show();
+      System.out.println("college  name:"+clgname);  
+    }
+}
+    
+
---
+++ q2.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab5;
+
+/**
+ *
+ * @author rakesh
+ */
+public class q2 {
+ 
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        bankA b1=new bankA();
+        System.out.println("Money deposited in bank A $"+b1.getBalance());
+        bankB b2=new bankB();
+        System.out.println("Money deposited in bank B $"+b2.getBalance());
+        bankC b3=new bankC();
+        System.out.println("Money deposited in bank C $"+b3.getBalance());
+       
+    }
+    
+}
+class bank 
+{
+    int getBalance()
+    {
+        return 0;
+    }
+}
+class bankA extends bank
+{
+    @Override
+    int getBalance()
+    {
+        return 1000;
+    }
+}
+class bankB extends bank
+{
+    @Override
+    int getBalance()
+    {
+        return 1500;
+    }
+}
+class bankC extends bank
+{
+    @Override
+    int getBalance()
+    {
+        return 2000;
+    }
+}
+
